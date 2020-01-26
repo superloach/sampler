b26f13b6ec1f627b300137fa61bdb536162fa8fa
Terra Brown <superloach42@gmail.com>
2020-01-26 18:04:43 -0500

    move 1<<15 from funcs to sampler

--- basics.go
+++ basics.go
@@ -5,24 +5,21 @@ import "math"
 type Func (func(*Sampler) float64)
 
 func Sine(s *Sampler) float64 {
-	pk := s.Volume * (1 << 15)
-	return pk * (math.Sin(2*math.Pi*(s.Frequency/float64(s.Rate))*float64(s.Index)) + 1) / 2
+	return s.Volume * (math.Sin(2*math.Pi*(s.Frequency/float64(s.Rate))*float64(s.Index)) + 1) / 2
 }
 
 func Saw(s *Sampler) float64 {
 	rf := float64(s.Rate) / s.Frequency
-	pk := s.Volume * (1 << 15)
-	return pk * (math.Mod(float64(s.Index), rf) / rf)
+	return s.Volume * (math.Mod(float64(s.Index), rf) / rf)
 }
 
 func Pulse(duty float64) Func {
 	return func(s *Sampler) float64 {
-		pk := s.Volume * (1 << 15)
-		sw := Saw(s) / pk
+		sw := Saw(s) / s.Volume
 		if sw > duty {
 			return 0
 		} else {
-			return pk
+			return s.Volume
 		}
 	}
 }
